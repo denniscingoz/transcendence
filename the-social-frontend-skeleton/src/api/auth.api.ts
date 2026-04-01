106c74315f619d32fddf9e8a63e5a0ecadd7fdce
denizcaglarcingoz <denizcaglarcingoz@gmail.com>
2026-04-01 21:32:43 +0200

    Some extra changes to areas

--- the-social-frontend-skeleton/src/api/auth.api.ts
+++ the-social-frontend-skeleton/src/api/auth.api.ts
@@ -1,7 +1,13 @@
 import api from './axios'
 import type { LoginRequest, LoginResponse } from '../types/api'
+import type { SignUpRequestDto, AuthResponseDto } from '../types/api'
 
 export async function login(payload: LoginRequest): Promise<LoginResponse> {
   const { data } = await api.post<LoginResponse>('/auth/login', payload)
+  return data
+}
+
+export async function signUp(request: SignUpRequestDto): Promise<AuthResponseDto> {
+  const { data } = await api.post<AuthResponseDto>('/signup', request)
   return data
 }
