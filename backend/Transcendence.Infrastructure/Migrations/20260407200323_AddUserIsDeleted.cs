fd261cd554b948e50f1f0f718ffcd5b059744199
grinietta <d.padenkova@gmail.com>
2026-04-07 22:17:11 +0200

    IsDeleted, LastMessageAt, LastMessageText added to migration

---
+++ backend/Transcendence.Infrastructure/Migrations/20260407200323_AddUserIsDeleted.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transcendence.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIsDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "app",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastMessageAt",
+                schema: "app",
+                table: "Conversations",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastMessageText",
+                schema: "app",
+                table: "Conversations",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "app",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "LastMessageAt",
+                schema: "app",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "LastMessageText",
+                schema: "app",
+                table: "Conversations");
+        }
+    }
+}
