9af1aeb0b9de76809ec561e37c392b2d9374566e
grinietta <d.padenkova@gmail.com>
2026-04-14 16:23:36 +0200

    migration for AvatarFileId uniquenece update, first .dbml file for schema not updated

---
+++ backend/Transcendence.Infrastructure/Migrations/20260414134615_AddUniqueIndexOnUsersAvatarFileId.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transcendence.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexOnUsersAvatarFileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_AvatarFileId",
+                schema: "app",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_AvatarFileId",
+                schema: "app",
+                table: "users",
+                column: "AvatarFileId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_AvatarFileId",
+                schema: "app",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_AvatarFileId",
+                schema: "app",
+                table: "users",
+                column: "AvatarFileId");
+        }
+    }
+}
